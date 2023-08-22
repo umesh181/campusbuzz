2a2e26f22b09c939ec491e9b3922bc5e3b0c8cad
TheMakers404 <bobburiumesh18@gmail.com>
2023-08-23 00:37:55 +0530

    done

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -1,13 +1,27 @@
-import 'package:campusbuzz_mainui/categories.dart';
+
+import 'package:campusbuzz_mainui/event_detail_screen.dart';
 import 'package:campusbuzz_mainui/model/event.dart';
 import 'package:campusbuzz_mainui/screen/inside_categ_screen.dart';
 import 'package:flutter/material.dart';
-
 class EventScreen extends StatelessWidget {
-  const EventScreen({super.key,required this.Ctitle,required this.events});
+  const EventScreen({super.key,required this.Ctitle,required this.events,required this.onToggleFavorite
+  });
 
   final String Ctitle;
   final List<Event> events;
+    final void Function(Event event) onToggleFavorite;
+
+    void selectEvent(BuildContext context, Event meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => EventDetailScreen(
+          event: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +50,10 @@ class EventScreen extends StatelessWidget {
     if (events.isNotEmpty) {
       content = GridView.builder(
         itemCount: events.length,
-        itemBuilder: (ctx, index) => EventItem(event: events[index]), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (ctx, index) => EventItem(event: events[index], onSelectEvent: (event) {
+            selectEvent(context, event);
+          }, onToggleFavorite:onToggleFavorite,
+        ), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       );
     }
     return Scaffold(
